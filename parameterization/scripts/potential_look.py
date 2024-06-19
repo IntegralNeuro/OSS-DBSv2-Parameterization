1584c4a971dc808bea944487ae0920af7803124a
Philip Sabes <sabes@starfishneuro.com>
2024-06-19 10:55:30 -0700

    potential_look is now finished

--- parameterization/scripts/potential_look.py
+++ parameterization/scripts/potential_look.py
@@ -1,31 +1,37 @@
 import numpy as np
-import vtk
-from vtk.util.numpy_support import vtk_to_numpy
+import pyvista as pv
 import csv
 from matplotlib import pyplot as plt
 
 
-dir = 'results/electrode_1_20240618_182403'
+
+dir = 'results/electrode_1_20240619_100924'
 vtu_file = dir + '/potential.vtu'
 csv_file = dir + '/oss_potentials_Lattice.csv'
 
 # vtu
-reader = vtk.vtkXMLUnstructuredGridReader()
-reader.SetFileName(vtu_file)
-reader.Update()  # Needed because of GetScalarRange
-output = reader.GetOutput()
-datv = vtk_to_numpy(output.GetPointData().GetArray("potential_real"))
+mesh = pv.read(vtu_file)
+v_vtu = mesh['potential_real']
+p_vtu = mesh.points
 
 # csv
-datc = []
+# index,x-pt,y-pt,z-pt,potential,inside_csf,inside_encap,frequency
+vlist = []
+plist = []
 with open(csv_file) as f:
     reader = csv.reader(f, delimiter=',')
     for i, row in enumerate(reader):
         if i>1:
-            datc.append(float(row[4]))
-datc = np.array(datc)
-datv = np.array(datv)
+            vlist.append(float(row[4]))
+            plist.append(np.array([float(row[1]), float(row[2]), float(row[3])]))
+v_csv = np.array(vlist)
+p_csv = np.vstack(plist)
 
-plt.plot(datv, 'r')
-plt.plot(datc, 'b')
-plt.show()
+labels = ['x', 'y', 'z']
+for i in range(3):
+    plt.subplot(1, 3, i + 1)
+    plt.plot(p_vtu[:, i], v_vtu, 'b.', label='')
+    plt.xlabel(labels[i])
+    plt.plot(p_csv[:, i], v_csv, 'r.', label='CSV' )
+    plt.xlabel(labels[i])
+plt.show(block=True)
